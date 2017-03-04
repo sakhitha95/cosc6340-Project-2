c30e9cb4613111bf94e4498c29e01cd24f87e9d9
Devin Crane <devin.crane@gmail.com>
2017-03-04 11:38:29 -0600

    Makes testing easier
    Added some code that will make testing easier. Command line works, but
    this way we can easily create our own sql query or script file to test
    with.

--- dbms.cpp
+++ dbms.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+//string sqlQuery = "";
+string sqlQuery = "query";
+string scriptFile = "";
+//string scriptFile = "file";
+
 void parseScriptFile(string scriptFile) {
     cout << scriptFile << endl;
 }
@@ -14,6 +19,12 @@ void commandLineSQLInput(string SQL) {
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         cout << "usage: dbms script=<filename> OR dbms \"<SQL query>\"" << endl;
+        if (!sqlQuery.empty()) {
+            commandLineSQLInput(sqlQuery);
+        } else if (!scriptFile.empty()) {
+            parseScriptFile(scriptFile);
+        }
+        return 0;
     }
 
     string firstArg = argv[1];
