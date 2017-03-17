b1d0d5c84c759895cebd5e560b49d0a14abf5807
rezash1 <amirreza@amirrezas-MacBook-Pro.local>
2017-03-17 11:44:46 -0500

    Working 3 funcs correctly!!!

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/devincrane/CLionProjects/DBMS
+# Install script for directory: /Users/amirreza/CLionProjects/project2/project2-desktop/cosc6340-Project-2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/devincrane/CLionProjects/DBMS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/amirreza/CLionProjects/project2/project2-desktop/cosc6340-Project-2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
